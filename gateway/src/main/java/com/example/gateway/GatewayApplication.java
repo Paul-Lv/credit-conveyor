0a03f0fd31c3c20cc919c32a23a2f1100a8e5b24
pul23 <tmp@my.com>
2026-03-13 22:15:29 +0400

    MVP Level-5

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,3 +1,5 @@
+package com.example.gateway;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
